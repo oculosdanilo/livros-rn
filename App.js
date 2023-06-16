c52a6403bfe929de3f9054b62679c1a9a3540556
oculosdanilo <oculosdanilo@gmail.com>
2023-06-16 17:25:52 -0300

    06/16/2023 17:25:52

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, View } from "react-native";
+import Header from "./src/components/header/index";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    /* <SafeAreaView> */
+      <View style={styles.container}>
+        <Header></Header>
+      </View>
+    /* </SafeAreaView> */
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    alignItems: "center",
   },
 });
